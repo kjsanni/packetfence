390aaf3e4f511b7c936fa29cfd626e881f72d8ad
Darren Satkunas <darren@satkunas.com>
2018-07-20 11:16:04 -0400

    Minor layout changes (#2)

--- html/pfappserver/root/static.alt/src/views/Nodes/index.vue
+++ html/pfappserver/root/static.alt/src/views/Nodes/index.vue
@@ -16,8 +16,6 @@
                             <div class="bd-toc-link" v-t="'Standard Searches'"></div>
                             <b-nav-item @click.stop :to='{"path":"search", "query":{"query":JSON.stringify({"op":"and","values":[{"op":"or","values":[{"field":"violation.open_count","op":"greater_than_equals","value":"1"}]}]})}}' replace>{{ $t('Open Violations') }}</b-nav-item>
                             <b-nav-item @click.stop :to='{"path":"search", "query":{"query":JSON.stringify({"op":"and","values":[{"op":"or","values":[{"field":"violation.close_count","op":"greater_than_equals","value":"1"}]}]})}}' replace>{{ $t('Closed Violations') }}</b-nav-item>
-
-                            <hr/>
                             <b-nav-item @click.stop :to='{"path":"search", "query":{"query":JSON.stringify({"op":"and","values":[{"op":"or","values":[{"field":"online","op":"not_equals","value":"on"}]}]})}}' replace>{{ $t('Offline Nodes') }}</b-nav-item>
                             <b-nav-item @click.stop :to='{"path":"search", "query":{"query":JSON.stringify({"op":"and","values":[{"op":"or","values":[{"field":"online","op":"equals","value":"on"}]}]})}}' replace>{{ $t('Online Nodes') }}</b-nav-item>
 
@@ -67,6 +65,7 @@
                             </b-collapse>
 
                           </b-nav>
+                          <hr/>
                         <pf-saved-search :storeName="this.$options.name.toLowerCase()" :routeName="this.$options.name.toLowerCase()"/>
                     </div>
                 </b-collapse>
